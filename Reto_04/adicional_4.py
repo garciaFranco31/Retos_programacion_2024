48c97082c151be03671f67fda5dc16fb7761bdfd
Franco Garcia Carril <garciacfranco2001@gmail.com>
2025-04-03 23:34:11 -0300

    ejercicio adicional

---
+++ Reto_04/adicional_4.py
@@ -0,0 +1,20 @@
+""" * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que analice dos palabras diferentes y realice comprobaciones
+ * para descubrir si son:
+ * - Palíndromos
+ * - Anagramas
+ * - Isogramas"""
+
+# "cadena" != "anedac" - no es palindromo
+# "oso" = "oso" - es palindromo
+
+
+def esPalindromo(word1):
+    if (word1.lower() == word1[::-1].lower()):
+        return True
+    else:
+        return False
+
+s = "Oso"
+print(esPalindromo(s))
+
